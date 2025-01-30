2b90f0ca9cddd257d302c6d323eeee55c986385d
BappiDeb <bappy08deb@gmail.com>
2025-01-31 05:37:33 +0600

    now

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5002;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -77,5 +77,4 @@ app.delete('/menu-items/:id', (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-    console.log(`You can test the API at http://localhost:${PORT}/menu-items`);
 });
